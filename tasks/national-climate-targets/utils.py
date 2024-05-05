e94b15e0a84cc2c5dd0ece0e4255322d01224e70
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-05-05 04:55:41 -0400

    evaluate in post-processing to allow comparison of single-label classifiers

--- tasks/national-climate-targets/utils.py
+++ tasks/national-climate-targets/utils.py
@@ -1,6 +1,8 @@
 import datasets
+import numpy as np
 
-_DOC_TO_CHOICE = [
+# TODO: replace choices with single letter
+_CHOICES = [
   'None', # 0
   'Reduction', # 1
   'Net zero', # 2
@@ -10,6 +12,12 @@ _DOC_TO_CHOICE = [
   'Net zero, Other', # 6
   'Reduction, Net zero, Other' # 7
 ]
+
+def _extract_labels(doc):
+    reduction = doc["annotation_Reduction"]
+    net_zero = doc["annotation_NZT"]
+    other = doc["annotation_Other"]
+    return reduction, net_zero, other
 
 def _labels_to_choice(reduction, net_zero, other) -> int:
     assert reduction in [0,1]
@@ -50,13 +58,26 @@ def _labels_to_choice(reduction, net_zero, other) -> int:
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
-        reduction = doc["annotation_Reduction"]
-        net_zero = doc["annotation_NZT"]
-        other = doc["annotation_Other"]
-
         output = {
-            'label': _labels_to_choice(reduction, net_zero, other)
+            'choices': _CHOICES
         }
         return output
 
     return dataset.map(_process_doc)
+
+def process_results_mcq(doc, results, true_labels):
+    results = [result[0] for result in results]
+
+    acc = 1.0 if int(np.argmax(results)) in true_labels else 0.0
+    completion_len = np.array([float(len(i)) for i in doc["choices"]])
+    acc_norm = 1.0 if int(np.argmax(results / completion_len)) in true_labels else 0.0
+
+    return {
+        "acc": acc,
+        "acc_norm": acc_norm,
+    }
+
+def process_results_total(doc, results):
+    reduction, net_zero, other = _extract_labels(doc)
+    choice_index = _labels_to_choice(reduction, net_zero, other)
+    return process_results_mcq(doc, results, [choice_index])
